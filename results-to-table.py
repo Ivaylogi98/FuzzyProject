29b8d4307815bf73c016e0143528ef1f6f97c7db
Ivaylo Ivanov <ivaylogi98@gmail.com>
2022-03-01 12:36:19 +0200

    Minor changes

--- results-to-table.py
+++ results-to-table.py
@@ -1,34 +1,36 @@
 import os
 import pandas as pd
 
-files = os.listdir('./output')
-# print(files)
+def main():
+    files = os.listdir('./output')
 
-results = []
-for fname in files:
-    if 'fuzzy' in fname:
-        with open('./output/' + fname, 'r') as f:
-            val = f.readline().split('(')[0]
-            k = fname.split('_')[2].split('=')[1]
-            m = fname.split('_')[3].split('=')[1]
-            rand = 10
-            if 'rand' not in fname:
-                m = m.split('.txt')[0]
-            else:
-                rand = fname.split('_')[4].split('=')[1].split('.')[0]
-            results.append([int(k), float(m), int(rand), float(val)])
+    results = []
+    for fname in files:
+        if 'fuzzy' in fname:
+            with open('./output/' + fname, 'r') as f:
+                val = f.readline().split('(')[0]
+                k = fname.split('_')[2].split('=')[1]
+                m = fname.split('_')[3].split('=')[1]
+                rand = 10
+                if 'rand' not in fname:
+                    m = m.split('.txt')[0]
+                else:
+                    rand = fname.split('_')[4].split('=')[1].split('.')[0]
+                results.append([int(k), float(m), int(rand), float(val)])
 
-df_list = pd.DataFrame(results, columns=['k','m','rand','val'])
-unique_k = set(sorted(df_list.loc[:, 'k']))
-unique_m = set(sorted(df_list.loc[:, 'm']))
-print(unique_k, unique_m)
+    df_list = pd.DataFrame(results, columns=['k','m','rand','val'])
+    unique_k = set(sorted(df_list.loc[:, 'k']))
+    unique_m = set(sorted(df_list.loc[:, 'm']))
+    print(unique_k, unique_m)
 
-table = pd.DataFrame(index=unique_k, columns=unique_m)
+    table = pd.DataFrame(index=unique_k, columns=unique_m)
 
-for row in results:
-    table.at[row[0], row[1]] = row[3]
+    for row in results:
+        table.at[row[0], row[1]] = row[3]
 
-print(table)
+    print(table)
 
+    table.to_excel('./output/table.xlsx')
 
-table.to_excel('./output/table.xlsx')
+if __name__ == '__main__':
+    main()
